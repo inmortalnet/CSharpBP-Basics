cbfdddc8fc6a5cd22b38a4c9f76bcfa570e59536
Carlos Ponce <carlos_ponce@outlook.com>
2016-04-09 21:31:47 -0700

    Added Product Class
    Added new Product Class and a Unit Test for it's Print method.

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public string Print()
+        {
+            return String.Format("[{0}] {1}: {2}", Id, Name, Description);
+        }
+
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            // Arrange
+            var currentProduct = new Product();
+            currentProduct.Id = 0;
+            currentProduct.Name = "Hammer";
+            currentProduct.Description = "Hack away with this shiny tool.";
+
+            var expected = "[0] Hammer: Hack away with this shiny tool.";
+
+            // Act
+            var actual = currentProduct.Print();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
